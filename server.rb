f464af87a55501d9949720052b73b938b5912e73
tasconajoey <joey.tascona@gmail.com>
2017-01-24 14:57:46 -0500

    Finsh full assignment

--- server.rb
+++ server.rb
@@ -15,8 +15,26 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  filename = "index.html"
-  response = File.read(filename)
+  #filename = "index.html"
+
+  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+
+  if File.exists?(filename)
+    response_body = File.read(filename)
+  else
+    response_body = "File Not Found\n" # need to indicate end of the string with \n
+  end
+
+  success_header = []
+  success_header << "HTTP/1.1 200 OK"
+  success_header << "Content-Type: text/html" # should reflect the appropriate content type (HTML, CSS, text, etc)
+  success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+  success_header << "Connection: close"
+  header = success_header.join("\r\n")
+
+  response = [header, response_body].join("\r\n\r\n")
+
+  #response = File.read(filename)
 
   client.puts(response)                       # Output the current time to the client
   client.close                                      # Disconnect from the client
